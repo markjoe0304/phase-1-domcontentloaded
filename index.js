3d506ac60ac934e87da32c82198d69db301d97e8
markjoe0304 <markjoegicheru@gmail.com>
2024-07-16 14:54:34 +0300

    moded

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+    const text = this.getElementById("text");
+  
+      text.textContent = "This is really cool!";
+  
+  
+  
+  
+  
+  
+  });
